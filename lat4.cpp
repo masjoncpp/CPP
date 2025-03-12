a7c73288e68aac7abfb18fada8d526ff2dc702ea
masjottie <111202416048@mhs.dinus.ac.id>
2025-03-12 12:22:36 +0700

    lat4.cpp

--- lat4.cpp
+++ lat4.cpp
@@ -4,40 +4,46 @@
 //Date : 5 Maret 2025
 #include<iostream>
 using namespace std;
+
 //Kamus
-    struct SGTG
-    {
-      //astribut
-        int als,tgg;
-    //method
-        float luas_sgtg(int A, int T)
+struct SGTG
 {
-      // kamus lokal
+    //atribut
+    int als, tgg;
+
+    //method
+    float luas_sgtg(int A, int T)
+    {
+        // kamus lokal
         float L;
 
-      //deskripsi function luas_sgtg
-        L=A*T*0.5;
+        //deskripsi function luas_sgtg
+        L = A * T * 0.5;
         return L;
     }
-    };
-    struct SGTG mendoan;
-    int luas;
+};
 
 //Deskripsi
-main()
+int main()
 {
-        //Judul Aplikasi
-        cout<< "Menghitung Luas Segitiga dengan fungsi luas"<<endl;
+    //Judul Aplikasi
+    cout << "Menghitung Luas Segitiga dengan fungsi luas" << endl;
 
-        //User mengisi data di aplikasi
-        cout<<"berapa nilai alasnya? :";cin>>mendoan.als;
-        cout<<"berapa nilai tingginya? :";cin>>mendoan.tgg;
+    //Deklarasi variabel
+    SGTG mendoan;
+    int alas, tinggi;
+    float luas;
 
-        //rumus menghitung luas segitiga dengan memanggil function/blackbox
-        luas=mendoan.luas_sgtg(als, tgg);
+    //User mengisi data di aplikasi
+    cout << "Berapa nilai alasnya? : "; cin >> alas;
+    cout << "Berapa nilai tingginya? : "; cin >> tinggi;
 
-        //Program menampilkan Hasil
-        cout<<"Luas Segitiga dengan alas :"<<alas<<"tinggi :"<<tinggi<<endl;
-        cout<<"adalah : "<<luas;
-return 0;
+    //rumus menghitung luas segitiga dengan memanggil function/blackbox
+    luas = mendoan.luas_sgtg(alas, tinggi);
+
+    //Program menampilkan Hasil
+    cout << "Luas Segitiga dengan alas : " << alas << " dan tinggi : " << tinggi << endl;
+    cout << "adalah : " << luas << endl;
+
+    return 0;
 }
